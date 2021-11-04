5874430dfeeb279c45172e0159bc869271e251d6
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-11-04 15:51:40 -0400

    Adding code for storing the data when the Application is in live and clear the data from the store (Cache)

--- public/db.js
+++ public/db.js
@@ -32,3 +32,34 @@ request.onupgradeneeded = ({ target }) => {
   
     store.add(record);
   }
+  //Checking the database, If the connection Is ON (ONLINE) then, establishing a connection and storing data in the database. For that, they are calling Api Post method.
+  function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {        
+          return response.json();
+        })
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", checkDatabase);
